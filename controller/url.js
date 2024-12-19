c1b97588bf5dfac6e3f85b29e53e184a1df71a38
Ajijur <ajijurrahmansujon@gmail.com>
2024-12-19 12:32:23 +0600

    added design to index.html and fix controller acording to index file

--- controller/url.js
+++ controller/url.js
@@ -2,13 +2,13 @@ import User from "../models/User.js";
 import shortid from "shortid";
 
 async function userUrl(req, res) {
-    const { url } = req.body;
+    const { redirectUrl } = req.body;
 
     const short = shortid();
     try {
-        const user = new User({ shortId: short, redirectUrl: url })
+        const user = new User({ shortId: short, redirectUrl })
         await user.save();
-        
+        res.status(201).json({ user: "create succesfully" })
 
     } catch (err) {
         res.status(400).json({ user: "bad request" });
